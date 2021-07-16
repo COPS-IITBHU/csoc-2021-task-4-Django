adeb30a8a316ad7abb1bf1037793d3caab7b7d63
Tarun-Arora <tarun.arora.cse20@itbhu.ac.in>
2021-07-16 18:03:50 +0530

    Completed required tasks

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path,include
 from store.views import *
 
 urlpatterns = [
     path('', index, name="index"),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('books/', bookListView, name="book-list"),
     path('book/<int:bid>/', bookDetailView, name='book-detail' ),
     path('books/loaned/', viewLoanedBooks, name="view-loaned"),
     path('books/loan/', loanBookView, name="loan-book"),
+    path('books/rate/', rateBookView, name="rate-book"),
     path('books/return/', returnBookView, name="return-book"),
 ]

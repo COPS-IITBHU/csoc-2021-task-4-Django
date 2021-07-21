4631a4d3a6d1b74a90e9ffe81a2924ae0422e5ba
tiger-yash <tigeryashraj@gmail.com>
2021-07-21 11:56:20 +0530

    AH

--- library/settings.py
+++ library/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '==e^87*tib$+f)9$46#-jc&toydks5v^g5ym7wq^shf5(hno3&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-library-task.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
